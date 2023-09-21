3d1fed22bd87de21d64a7a3c28ad7b18bc8ce991
NicolasBourassin <bourassinnicolas@gmail.com>
2023-09-21 22:29:08 +0200

    individual toggle likes of postings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import VideoComponent from "../postings/VideoComponent";
-export default function Home(){
 
-    const [postings, setPostings]=useState([])
-    const [users, setUsers]=useState([])
-    const [comments, setComments]=useState([])
+export default function Home() {
+    const [postings, setPostings] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [comments, setComments] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         loadPostings();
         loadUsers();
         loadComments();
@@ -17,24 +17,30 @@ export default function Home(){
         try {
             const resultPostings = await axios.get("http://localhost:8080/postings");
             console.log(resultPostings.data); // Log the data to check if it's loaded correctly
-            setPostings(resultPostings.data);
+            // Add a like status property to each posting
+            const postingsWithLikes = resultPostings.data.map((posting) => ({
+                ...posting,
+                isLiked: false, // Initialize as not liked
+            }));
+            setPostings(postingsWithLikes);
         } catch (error) {
             console.error("Error loading postings:", error);
         }
     };
-    const loadUsers=async()=>{
-        const resultUsers=await axios.get("http://localhost:8080/users");
+
+    const loadUsers = async () => {
+        const resultUsers = await axios.get("http://localhost:8080/users");
         setUsers(resultUsers.data);
-    }
+    };
 
-    const loadComments=async()=>{
-        const resultComments=await axios.get("http://localhost:8080/comments");
+    const loadComments = async () => {
+        const resultComments = await axios.get("http://localhost:8080/comments");
         setComments(resultComments.data);
-    }
+    };
 
-    const getUserId = (user) =>{
+    const getUserId = (user) => {
         return user ? user.id : null;
-    }
+    };
 
     const getPostingAuthorUsername = (authorId) => {
         const author = users.find((user) => user.id === authorId);
@@ -46,9 +52,20 @@ export default function Home(){
         return author ? author.photoUrl : "";
     };
 
-    return(
-        <div className='container'>
-            <div className='py-4'>
+    // Function to handle toggling the like status
+    const toggleLike = (posting) => {
+        setPostings((prevPostings) =>
+            prevPostings.map((prevPosting) =>
+                prevPosting.id === posting.id
+                    ? { ...prevPosting, isLiked: !prevPosting.isLiked }
+                    : prevPosting
+            )
+        );
+    };
+
+    return (
+        <div className="container">
+            <div className="py-4">
                 <table className="table bg-body-secondary">
                     <thead>
                     <tr>
@@ -60,64 +77,55 @@ export default function Home(){
                     </tr>
                     </thead>
                     <tbody>
-                    {postings.length > 0 && postings.map((posting, index) => (
-                        <tr key={posting.id}>
+                    {postings.length > 0 &&
+                        postings.map((posting, index) => (
+                            <tr key={posting.id}>
+                                <th scope="row">{index + 1}</th>
+                                <td>
+                                    {posting.author ? (
+                                        <img
+                                            src={getPostingAuthorPhotoUrl(getUserId(posting.author))}
+                                            alt="profile pic"
+                                            width="50"
+                                        />
+                                    ) : (
+                                        <img src="/character.png" alt="profile pic" width="50" />
+                                    )}
+                                </td>
+                                <td>
+                                    {posting.author
+                                        ? getPostingAuthorUsername(getUserId(posting.author))
+                                        : "Unknown author"}
+                                </td>
 
-                            <th scope="row">{index + 1}</th>
-                            <td>
-                                {posting.author
-                                    ? <img src={getPostingAuthorPhotoUrl(getUserId(posting.author))}
-                                           alt="profile pic" width="50" />
-                                    : <img src="/character.png"
-                                           alt="profile pic" width="50" />}
-                            </td>
-                            <td>
-                                {posting.author ? getPostingAuthorUsername(getUserId(posting.author)) : "Unknown author"}
-                            </td>
-
-                            <td>
-                                {posting.videoType ? (                                                // // syntax 1 : for mp4 format
-                                    // <video width="320" height="240" controls>
-                                    //     <source src={posting.urlContent} type="video/mp4" />
-                                    //     Your browser does not support the video tag.
-                                    // </video>
-
-                                    // // syntax 2
-                                    // // Works but security prevents video to be displayed in the navigator
-                                    <iframe
-                                        width="560"
-                                        height="315"
-                                        src={posting.urlContent}
-                                        title="YouTube video player"
-                                        frameBorder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                        allowFullScreen
-                                    ></iframe>
-
-                                    // // syntax 3 : attempt to display correctly youtube content
-                                    // <VideoComponent videoUrl={posting.urlContent} />
-                                ) : (
-                                    <img src={posting.urlContent} alt="Image" width="200" />
-                                )}
-                            </td>
-                            <td> <button className="btn">
-                                {true ?
-                                <img src={"./thumb-up_reverse.png"} width="30"/>
-                                :
-                                <img src={"./thumb-up.png"} width="30"/>}
-                            </button> </td>
-                            {/* TODO: affichage vidéo/image selon booléen */}
-                            {/* TODO: récupération et affichage des commentaires */}
-                        </tr>
-                    ))}
-                    </tbody>                </table>
-                {/*    NOTE : syntax for youtube clean integration*/}
-
-                {/*<iframe width="640" height="360" src="https://www.youtube.com/embed/J7pBztjUqUc"*/}
-                {/*        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"*/}
-                {/*        allowFullScreen></iframe>*/}
-
-
+                                <td>
+                                    {posting.videoType ? (
+                                        <iframe
+                                            width="560"
+                                            height="315"
+                                            src={posting.urlContent}
+                                            title="YouTube video player"
+                                            frameBorder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowFullScreen
+                                        ></iframe>
+                                    ) : (
+                                        <img src={posting.urlContent} alt="Image" width="200" />
+                                    )}
+                                </td>
+                                <td>
+                                    <button className="btn" onClick={() => toggleLike(posting)}>
+                                        {posting.isLiked ? (
+                                            <img src="./thumb-up_reverse.png" width="30" />
+                                        ) : (
+                                            <img src="./thumb-up.png" width="30" />
+                                        )}
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
